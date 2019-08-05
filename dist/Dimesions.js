ec86f8e27d958d00357b86087c4bf2d2eb08461a
Fernando-De-Santiago <47462421+Fernando-De-Santiago@users.noreply.github.com>
2019-08-05 13:34:33 -0600

    Add files via upload

---
+++ dist/Dimesions.js
@@ -0,0 +1,10 @@
+var select = document.getElementById("roofs")
+    arr=["12, 21 7","12, 21 8","Vertical"]
+    for(var i=0;i<arr.length;i++)
+    {
+        var option = document.createElement("OPTION"),
+            txt=document.createTextNode(arr[i]);
+            option.appendChild(txt);
+            option.setAttribute("value",arr[i])
+            select.insertBefore(option,select.lastChild);
+    }
